da7cb05047bab2f9058f9b589ad4035c932d03df
Vaibhav Bali <vibsbali@gmail.com>
2016-09-21 20:06:55 +1000

    Added hold and wait pattern construct

---
+++ SessionManagement.ThreadingSynch/Bakery.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace SessionManagement.ThreadingSync
+{
+    public class Bakery
+    {
+        private readonly Queue<Donut> donutTray = new Queue<Donut>();
+
+        public Donut GetDonut()
+        {
+            lock (donutTray)
+            {
+                while (donutTray.Count == 0)
+                {
+                    Monitor.Wait(donutTray);
+                }
+
+                return donutTray.Dequeue();
+            }
+        }
+
+        public void AddDonut(Donut[] freshDonuts)
+        {
+            lock (donutTray)
+            {
+                foreach (var donut in freshDonuts)
+                {
+                    donutTray.Enqueue(donut);
+                }
+
+                Monitor.PulseAll(donutTray);
+            }
+        }
+    }
+
+    public class Donut
+    {
+        public Donut()
+        {
+            Name = "Donut yumm!";
+        }
+        public string Name { get; set; }
+    }
+}
---
+++ SessionManagement.ThreadingSynch/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading;
+
+namespace SessionManagement.ThreadingSync
+{
+    class Program
+    {
+        private static object locker = new object();
+
+        private static int sum = 0;
+
+        static void Main(string[] args)
+        {
+            //Create a donut factory
+            var people = new int[100];
+
+            var bakery = new Bakery();
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < 30; i++)
+                {
+                    Console.WriteLine(bakery.GetDonut().Name);
+                }
+            }).Start();
+
+
+            for (int i = 0; i < 100; i++)
+            {
+                var donuts = new Donut[1];
+                donuts[0] = new Donut();
+                bakery.AddDonut(donuts);
+            }
+
+
+            var threads = new Thread[10];
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(AddOne);
+                threads[i].Start();
+                threads[i] = new Thread(SubstractOne);
+                threads[i].Start();
+            }
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Join();
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        static void AddOne()
+        {
+
+            //Monitor.Enter(locker);
+            lock (locker)
+            {
+                int temp = sum;
+                temp++;
+                Thread.Sleep(1);
+                sum = temp;
+                Console.WriteLine($"After adding 1 {sum}");
+                //Interlocked.Add(ref sum, 2);
+            }
+            //Monitor.Exit(locker);
+        }
+
+        static void SubstractOne()
+        {
+            lock (locker)
+            {
+                int temp = sum;
+                temp--;
+                Thread.Sleep(1);
+                sum = temp;
+                Console.WriteLine($"After substracting 1 {sum}");
+            }
+        }
+    }
+}
